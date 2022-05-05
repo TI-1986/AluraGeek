c1d2118e7769e76dd66bf6d02f65265094e7e5f7
TI-1986 <tiagohanrysites@gmail.com>
2022-05-04 22:00:51 -0300

    tela de cadastro do prooduto funcao captura imagem

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,30 @@
-function legenda() {
-  const canvas = document.getElementById('canvas').getContext('2D')
+function upload() {
+  canvas = createCanvas(192, 128)
+  backgroud(128)
+  canvas.drop(loadImgCanvas)
 
-  canvas.font = '10px Raleway'
+  Input = select('#canvas')
+  input.dragOver(activate)
+  input.draLeave(deactivate)
 
-  canvas.fillText('Arraste para adicionar uma imagem para o produto', 10, 50)
+  input.drop(load_img, deactivate)
+}
+function activate() {
+  input.style('background-color', '#888')
+}
+function deactivate() {
+  input.style('background-color', '#888')
+}
+
+function load_img(file) {
+  createP(file.data + '' + file.size)
+  var img = createImg(file.data)
+  img.size(192, 128)
+}
+
+function loadImgCanvas(file) {
+  loadImage(file.data, img => {
+    resizeCanvas(500)
+    image(img, 0, 0, width, heigth)
+  })
 }
